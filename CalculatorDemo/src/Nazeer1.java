8bc649ff12b0ea9603bf8ab7d3f0e36bd34f720e
deekshith1705 <66842754+deekshith1705@users.noreply.github.com>
2020-09-17 20:55:15 +0530

    Update Nazeer1.java

--- CalculatorDemo/src/Nazeer1.java
+++ CalculatorDemo/src/Nazeer1.java
@@ -3,9 +3,10 @@ public class Nazeer1 {
 
 	   public static void main(String[] args) {
 	        
-	      int num1 = 5, num2 = 15, sum;
+	      int num1 = 5, num2 = 15, sum,sub;
 	      sum = num1 + num2;
-
+	      sub = num2 - num1;
+	      System.out.println("Subtraction of these numbers: "+sub);
 	      System.out.println("Sum of these numbers: "+sum);
 	   }
 	}
